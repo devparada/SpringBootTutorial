f5292ea8dd0fb2020e8c63a5194ffad6c6e42294
Raúl <raul.parada040103@gmail.com>
2025-01-13 17:14:38 +0100

    Creado el hola mundo y el saludo con nombre

---
+++ src/main/java/tutoSpringBoot/controllers/HelloWorldRestController.java
@@ -0,0 +1,15 @@
+package tutoSpringBoot.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldRestController {
+
+    @GetMapping({ "/hello", "/hw", "/hola" })
+    public String helloWorld() {
+        System.out.println("Solicitud ejecutada!");
+        return "Hello, World!";
+    }
+
+}
