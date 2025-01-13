00cabfdada75d744ad3cd8532029e06da94d1414
Raúl <raul.parada040103@gmail.com>
2025-01-13 15:10:30 +0100

    Creación del proyecto tutoSpringBoot

---
+++ src/main/java/tutoSpringBoot/TutoConSpringBootApplication.java
@@ -0,0 +1,13 @@
+package tutoSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutoConSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutoConSpringBootApplication.class, args);
+	}
+
+}
