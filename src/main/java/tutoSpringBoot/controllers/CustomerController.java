305374be9b7768ef1121dd1f86256949441a12b8
Raúl <raul.parada040103@gmail.com>
2025-01-14 16:52:09 +0100

    Implementación de una API REST

---
+++ src/main/java/tutoSpringBoot/controllers/CustomerController.java
@@ -0,0 +1,26 @@
+package tutoSpringBoot.controllers;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import tutoSpringBoot.domain.Customer;
+
+@RestController
+public class CustomerController {
+
+    private List<Customer> customers = new ArrayList<>(Arrays.asList(
+            new Customer(123, "Gerardo López", "gerardol", "contrasena123"),
+            new Customer(456, "Alejandra García", "alegarcia", "clave456"),
+            new Customer(789, "Laura Sanchéz", "lauras", "secreto789"),
+            new Customer(234, "Carlos Martínez", "carlosm", "password234")));
+
+    @GetMapping("/clientes")
+    public List<Customer> getCustomers() {
+        return customers;
+    }
+
+}
